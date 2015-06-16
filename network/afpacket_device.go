01f5d0ad33deeaa39d7015ddc4118c9197fbe857
David Pariag <dpariag@sandvine.com>
2015-06-16 15:19:39 -0500

    More afpacket fixes

--- network/afpacket_device.go
+++ network/afpacket_device.go
@@ -1,17 +1,36 @@
+//+build linux
+
 package network
 
 import (
+	"github.com/google/gopacket"
 	"github.com/google/gopacket/afpacket"
 )
 
-type afpacketDeviceHandle struct {
-	tpacket	TPacket
+// Implements the ioHandle interface for packet I/O
+type afpacketIOHandle struct {
+	tpacket *afpacket.TPacket
 }
 
-func (h *afpacketDeviceHandle) ReadPacketData() (data []byte, ci gopacket.CaptureInfo, err error) {
+func (h *afpacketIOHandle) ReadPacketData() (data []byte, ci gopacket.CaptureInfo, err error) {
 	return h.tpacket.ReadPacketData()
 }
 
-func (h *afpacketDeviceHandle) WritePacketData(data []byte) (err error) {
+func (h *afpacketIOHandle) WritePacketData(data []byte) (err error) {
 	return h.tpacket.WritePacketData(data)
+}
+
+func newAfPacketIOHandle(name string) (handle *afpacketIOHandle, err error) {
+	var af afpacketIOHandle
+	af.tpacket, err = afpacket.NewTPacket()
+	return &af, err
+}
+
+func NewAfPacketDevice(name string, role int) Device {
+	handle, err := newAfPacketIOHandle(name)
+	if err != nil {
+		panic(err)
+	}
+	d := newDevice(name, role, handle)
+	return d
 }
